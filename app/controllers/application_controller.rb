8e97049eb8327fbb606d0cb102fa697e21fb12fa
Mark Jones <aneeshkohli4@gmail.com>
2016-11-06 13:04:02 +0000

    first commit login/logout

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+
+
+ def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || home2_index_path
+ end
+
+end
